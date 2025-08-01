75f16f92b0ff28cecf8fad32c3bea3ff86a7807d
Andreas Mikko <a.mikko@gmail.com>
2025-08-01 17:31:56 +0200

    Take picture

--- app/src/main/java/com/example/sum200_androidhardware/CameraActivity.kt
+++ app/src/main/java/com/example/sum200_androidhardware/CameraActivity.kt
@@ -1,23 +1,31 @@
 package com.example.sum200_androidhardware
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
@@ -52,18 +60,13 @@ class CameraActivity : ComponentActivity() {
 
 @Composable
 fun CameraScreen(modifier: Modifier = Modifier) {
-    CameraPreview(modifier = modifier.fillMaxSize())
-    Button(
-        onClick = {}
-    ) {
-        Text("Take Picture")
-    }
-}
-
-@Composable
-fun CameraPreview(modifier: Modifier = Modifier) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
+
+    val picture = remember { mutableStateOf<Bitmap?>(null) }
+    val onPictureTaken = { bitmap: Bitmap ->
+        picture.value = bitmap
+    }
 
     val cameraController = remember {
         LifecycleCameraController(context).apply {
@@ -72,6 +75,30 @@ fun CameraPreview(modifier: Modifier = Modifier) {
         }
     }
 
+    CameraPreview(
+        modifier = modifier.fillMaxSize(),
+        cameraController = cameraController
+    )
+
+    Button(
+        onClick = {
+            takePicture(context, cameraController, onPictureTaken)
+        }
+    ) {
+        Text("Take Picture")
+    }
+
+    val pictureValue = picture.value
+    if (pictureValue != null) {
+        Image(
+            bitmap = pictureValue.asImageBitmap(),
+            "Picture taken"
+        )
+    }
+}
+
+@Composable
+fun CameraPreview(modifier: Modifier = Modifier, cameraController: CameraController) {
     AndroidView(
         factory = { ctx ->
             PreviewView(ctx).apply {
@@ -84,6 +111,17 @@ fun CameraPreview(modifier: Modifier = Modifier) {
         },
         modifier = modifier
     )
+}
+
+fun takePicture(context: Context, cameraController: CameraController, onPictureTaken: (Bitmap) -> Unit ) {
+    val executor = ContextCompat.getMainExecutor(context)
+    cameraController.takePicture(executor, object : ImageCapture.OnImageCapturedCallback() {
+        override fun onCaptureSuccess(image: ImageProxy) {
+            super.onCaptureSuccess(image)
+            val bitmap = image.toBitmap()
+            onPictureTaken(bitmap)
+        }
+    })
 }
 
 @Preview(showBackground = true)
